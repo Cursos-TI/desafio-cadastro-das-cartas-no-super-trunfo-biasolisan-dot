bffb64709113bc21d81a9b3bcbe7fd5ee1c0c24f
creci <biasolisan@gmail.com>
2025-08-18 13:06:28 +0000

    Desafio Xadrez

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
-    int main(){
-               
-          int idade, matricula;
-          float altura;
-          char nome[50];
 
-          printf("Digite sua idade:    \n");
-          scanf("%d", &idade);
+int main(){
+    printf("Desafio Xadrez! \n");
 
-          printf("Digite sua altura:    \n");
-          scanf("%2f", &altura);
+    char estado[50];
+    int codigoestado;
+    char cidade[50];
+    int populacao;
+    int area;
+    int PIB;
+    int pontosturisticos;
 
-          printf("Digite seu nome:    \n");
-          scanf("%s", &nome);
+        printf("Digite o estado:    \n");
+        scanf("%s",estado);
+        
+       printf("Codigo do estado: \n");
+       scanf("%d", &codigoestado);
 
+        printf("Digite o nome da cidade:     \n");
+        scanf("%s", cidade);
 
-          printf("Digite sua matrícula:     \n");
-          scanf( "%d", &matricula );
+        printf("digite o numero da populacao:   \n" );
+        scanf("%d", &populacao);
 
-              printf("nome do aluno: %s - matricula: %d", nome, matricula);
+        printf("Digite a area:    \n");
+        scanf("%d", &area);
 
-              printf("idade: %d - altura: %2f", idade, altura);
+        printf("Digite o PIB:    \n");
+        scanf("%d", &PIB );
 
-              return 0;
+        printf("Digite o numero de pontos turisticos:   \n");
+        scanf("%d", &pontosturisticos); 
+       
+        printf(" estado %s ", estado);
+        printf("codigoestado %d", codigoestado);
+        printf("cidade %s ", cidade);
+        printf("populacao %d ", populacao);       
+        printf("area %d ", area);
+        printf("PIB %d", PIB);
+        printf("pontosturisticos %d" - pontosturisticos);
+            
+return 0;
 
-    }
+}   
 
-
+         
